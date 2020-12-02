5d38ce4eabadfcce1b98e8278747dbff2010d983
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2020-12-02 16:48:52 +0000

    [LG][DLS-3499] deleted test.routes file and added it's occupants to testOnly....routes file

--- app/uk/gov/hmrc/helptosavereminder/controllers/test/TestController.scala
+++ app/uk/gov/hmrc/helptosavereminder/controllers/test/TestController.scala
@@ -17,7 +17,9 @@
 package uk.gov.hmrc.helptosavereminder.controllers.test
 
 import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.helptosavereminder.repo.{HtsReminderMongoRepository, HtsReminderRepository}
 import uk.gov.hmrc.helptosavereminder.services.test.TestService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
@@ -25,12 +27,23 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @Singleton
-class TestController @Inject()(testService: TestService, cc: ControllerComponents) extends BackendController(cc) {
+class TestController @Inject()(
+  testService: TestService,
+  repository: HtsReminderMongoRepository,
+  cc: ControllerComponents)
+    extends BackendController(cc) {
 
   def populateReminders(noUsers: Int, emailPrefix: String, daysToReceive: Seq[Int]): Action[AnyContent] = Action.async {
     Future
       .sequence((0 until noUsers).map(_ => testService.generateAndInsertReminder(emailPrefix, daysToReceive)))
       .map(_ => Ok)
 
+  }
+
+  def getHtsUser(nino: String): Action[AnyContent] = Action.async { implicit request =>
+    repository.findByNino(nino).map {
+      case Some(htsUser) => Ok(Json.toJson(htsUser))
+      case None          => NotFound
+    }
   }
 }
