5b1e79286134c03f37056731c0b3adfceef45bb4
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2026-01-23 08:33:57 +0000

    [DLS-12615] upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,14 @@ import sbt.*
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val hmrcBootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val hmrcBootstrapVersion = "10.5.0"
+  private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion,
     "uk.gov.hmrc"            %% s"bootstrap-backend-$playVersion" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"            %% s"domain-$playVersion"            % "12.1.0",
+    "uk.gov.hmrc"            %% s"domain-$playVersion"            % "13.0.0",
+    "uk.gov.hmrc"            %% "domain-test-play-30"             % "13.0.0" ,
     "org.typelevel"          %% "cats-core"                       % "2.13.0",
     "io.github.samueleresca" %% "pekko-quartz-scheduler"          % "1.2.2-pekko-1.0.x"
   )
