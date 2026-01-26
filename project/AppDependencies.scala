740a5a7ed22c2eef8385fd97019a170511fcfbdd
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2026-01-26 21:29:02 +0000

    [DLS-12615] bot fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion,
     "uk.gov.hmrc"            %% s"bootstrap-backend-$playVersion" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"            %% s"domain-$playVersion"            % "13.0.0",
-    "uk.gov.hmrc"            %% "domain-test-play-30"             % "13.0.0" ,
+    "uk.gov.hmrc"            %% s"domain-test-$playVersion"       % "13.0.0" ,
     "org.typelevel"          %% "cats-core"                       % "2.13.0",
     "io.github.samueleresca" %% "pekko-quartz-scheduler"          % "1.2.2-pekko-1.0.x"
   )
